b64e0da8d2c225f93249ce5bbe8b6455d08ce10c
das2016 <bachouche.skander@gmail.com>
2020-05-11 02:09:22 +0200

    Commit : data rating service

---
+++ src/main/java/io/bestcoder/resources/Rating.java
@@ -0,0 +1,12 @@
+package io.bestcoder.resources;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Rating {
+
+	private String movieId;
+	private int rating;
+}
---
+++ src/main/java/io/bestcoder/resources/RatingResource.java
@@ -0,0 +1,25 @@
+package io.bestcoder.resources;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/ratings")
+public class RatingResource {
+
+	@RequestMapping("/{movieId}")
+	public Rating getRating(@PathVariable("movieId")String ratingId) {
+		return new Rating(ratingId, 4);
+	}
+	
+	@RequestMapping("/users/{userId}")
+	public UserRating getUserRating(@PathVariable("userId")String userId) {
+		return new UserRating(Arrays.asList(new Rating("1", 2)));
+	}
+	
+}
